98186282c8fc1319f8a70b99ca9f06973bccd406
Heedong Kim <ruthetum@gmail.com>
2021-10-19 16:59:09 +0900

    [feat] 프로젝트 셋업

---
+++ src/main/java/com/example/BlindCafe/BlindCafeApplication.java
@@ -0,0 +1,13 @@
+package com.example.BlindCafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlindCafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlindCafeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BlindCafe/BlindCafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BlindCafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlindCafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
