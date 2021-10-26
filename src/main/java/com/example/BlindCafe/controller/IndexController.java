10a5d69ba2acd8c0ae3cbe2ac82fdf817412a270
Heedong Kim <ruthetum@gmail.com>
2021-10-26 14:24:24 +0900

    [feat] 인덱스 페이지 추가

---
+++ src/main/java/com/example/BlindCafe/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.example.BlindCafe.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String home() {
+        log.info("GET /");
+        return "index";
+    }
+
+}
