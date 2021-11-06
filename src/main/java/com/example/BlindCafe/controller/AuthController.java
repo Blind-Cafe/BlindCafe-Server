53c648ff461ca13270fd322f418a00b1c21fd3a4
Heedong Kim <ruthetum@gmail.com>
2021-11-06 20:41:53 +0900

    [refactor] AuthController 분리

---
+++ src/main/java/com/example/BlindCafe/controller/AuthController.java
@@ -0,0 +1,34 @@
+package com.example.BlindCafe.controller;
+
+import com.example.BlindCafe.dto.LoginDto;
+import com.example.BlindCafe.service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+import static com.example.BlindCafe.type.Social.APPLE;
+import static com.example.BlindCafe.type.Social.KAKAO;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final UserService userService;
+
+    @PostMapping("/api/kakao")
+    public LoginDto.Response signupKakao(@Valid @RequestBody LoginDto.Request request) {
+        log.info("POST /api/kakao");
+        return userService.signin(request, KAKAO);
+    }
+
+    @PostMapping("/api/apple")
+    public LoginDto.Response signupApple(@Valid @RequestBody LoginDto.Request request) {
+        log.info("POST /api/apple");
+        return userService.signin(request, APPLE);
+    }
+}
--- src/main/java/com/example/BlindCafe/controller/UserController.java
+++ src/main/java/com/example/BlindCafe/controller/UserController.java
@@ -9,43 +9,26 @@ import com.example.BlindCafe.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 import static com.example.BlindCafe.config.SecurityConfig.getUserId;
-import static com.example.BlindCafe.type.Social.APPLE;
-import static com.example.BlindCafe.type.Social.KAKAO;
-
 @Slf4j
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/user")
 public class UserController {
 
     private final UserService userService;
 
-    @PostMapping("/api/kakao")
-    public LoginDto.Response signupKakao(@Valid @RequestBody LoginDto.Request request) {
-        log.info("POST /api/kakao");
-        return userService.signin(request, KAKAO);
-    }
-
-    @PostMapping("/api/apple")
-    public LoginDto.Response signupApple(@Valid @RequestBody LoginDto.Request request) {
-        log.info("POST /api/apple");
-        return userService.signin(request, APPLE);
-    }
-
-    @GetMapping("/api/user")
+    @GetMapping
     public UserDetailDto getUserDetail(Authentication authentication) {
         log.info("GET /api/user");
         return userService.getUserDetail(getUserId(authentication));
     }
 
-    @PostMapping("/api/user")
+    @PostMapping
     public CreateUserInfoDto.Response addUserInfo(
             Authentication authentication,
             @Valid @RequestBody CreateUserInfoDto.Request request
@@ -55,7 +38,7 @@ public class UserController {
         return userService.addUserInfo(user, request);
     }
 
-    @GetMapping("/api/user/home")
+    @GetMapping("home")
     public UserHomeDto.Response userHome(Authentication authentication) {
         log.info("GET /api/user/home");
         return userService.userHome(getUserId(authentication));
