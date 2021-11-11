22d14196d0cf05d49cafe5efc105a88915f1bbe8
Heedong Kim <ruthetum@gmail.com>
2021-11-11 20:21:43 +0900

    [refactor] AuthService 분리

--- src/main/java/com/example/BlindCafe/service/UserService.java
+++ src/main/java/com/example/BlindCafe/service/UserService.java
@@ -19,14 +19,9 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 import static com.example.BlindCafe.exception.CodeAndMessage.*;
-import static com.example.BlindCafe.auth.jwt.JwtUtils.createToken;
 import static com.example.BlindCafe.type.Gender.N;
-import static com.example.BlindCafe.type.Social.APPLE;
-import static com.example.BlindCafe.type.Social.KAKAO;
 import static com.example.BlindCafe.type.status.CommonStatus.*;
 import static com.example.BlindCafe.type.status.MatchingStatus.*;
-import static com.example.BlindCafe.auth.SocialUtils.*;
-import static com.example.BlindCafe.type.status.UserStatus.*;
 import static com.example.BlindCafe.type.status.UserStatus.NORMAL;
 
 @Service
@@ -46,81 +41,6 @@ public class UserService {
     private static int index;
     private static int editIndex;
 
-    @Transactional
-    public LoginDto.Response signin(LoginDto.Request request, Social social) {
-        LoginDto.SocialResponse socialResponse = getInfoByToken(request.getToken(), social);
-        String deviceId = request.getDeviceId();
-
-        Optional<User> userOptional = userRepository.findBySocialId(socialResponse.getSocialId());
-        boolean isRegistered = userOptional.isPresent();
-
-         if (isRegistered) {
-             User user = userOptional.get();
-             // 신고 유저
-             if (user.getStatus().equals(SUSPENDED))
-                 throw new BlindCafeException(SUSPENDED_USER);
-             else {
-                 // 디바이스 토큰 업데이트
-                 if (!user.getDeviceId().equals(deviceId)) {
-                     user.setDeviceId(deviceId);
-                     userRepository.save(user);
-                 }
-                 if (user.getStatus().equals(NOT_REQUIRED_INFO)) {
-                     // 필수 정보가 입력되지 않은 경우
-                     return getLoginResponse(user, SIGN_IN_NOT_REQUIRED_INFO);
-                 } else {
-                     // 필수 정보가 입력된 경우 정상 로그인
-                     return getLoginResponse(user, SIGN_IN);
-                 }
-             }
-         } else {
-             // 회원가입
-             User user = User.builder()
-                     .socialId(socialResponse.getSocialId())
-                     .socialType(socialResponse.getSocialType())
-                     .deviceId(deviceId)
-                     .status(UserStatus.NOT_REQUIRED_INFO)
-                     .build();
-             userRepository.save(user);
-             return getLoginResponse(user, SIGN_UP);
-         }
-    }
-
-    /**
-     * 엑세스 토큰으로 유저 정보 얻기
-     * @param token
-     * @return 유저 정보
-     */
-    private LoginDto.SocialResponse getInfoByToken(String token, Social social) {
-        if (social.equals(KAKAO)) {
-            // 카카오 로그인
-            return LoginDto.SocialResponse.builder()
-                    .socialId(verifyKakaoToken(token))
-                    .socialType(KAKAO)
-                    .build();
-        } else if (social.equals(APPLE)) {
-            return LoginDto.SocialResponse.builder()
-                    .socialId(verifyAppleToken(token))
-                    .socialType(APPLE)
-                    .build();
-        } else {
-            throw new BlindCafeException(INTERNAL_SERVER_ERROR);
-        }
-    }
-
-    /**
-     * 토큰 생성 및 반환하기
-     */
-    private LoginDto.Response getLoginResponse(User user, CodeAndMessage codeAndMessage) {
-        String token = createToken(user);
-        String nickname = user.getNickname() != null ? user.getNickname() : null;
-        return LoginDto.Response.builder()
-                .codeAndMessage(codeAndMessage)
-                .jwt(token)
-                .id(user.getId())
-                .nickname(nickname)
-                .build();
-    }
 
     @Transactional
     public CreateUserInfoDto.Response addUserInfo(User user, CreateUserInfoDto.Request request) {
