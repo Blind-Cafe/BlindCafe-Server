7be5420c402b5ec3e225673331379f88ed8489c3
Heedong Kim <ruthetum@gmail.com>
2021-11-14 22:17:09 +0900

    [feat] 신고 내역 조회 컨트롤러

--- src/main/java/com/example/BlindCafe/controller/ReportController.java
+++ src/main/java/com/example/BlindCafe/controller/ReportController.java
@@ -6,10 +6,7 @@ import com.example.BlindCafe.service.ReportService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -33,6 +30,17 @@ public class ReportController {
     ) {
         log.info("POST /api/report");
         return reportService.createReport(getUserId(authentication), request);
+    }
+
+    /**
+     * 신고 내역 확인하기
+     */
+    @GetMapping
+    public ReportListDto getReports(
+            Authentication authentication
+    ) {
+        log.info("GET /api/report");
+        return reportService.getReports(getUserId(authentication));
     }
 
 }
