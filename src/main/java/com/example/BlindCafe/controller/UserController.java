66a4d2aedbf74e9e667c7fef24c510bf2eaac2bf
Heedong Kim <ruthetum@gmail.com>
2021-11-22 00:22:56 +0900

    [feat] UserController http status 추가

--- src/main/java/com/example/BlindCafe/controller/UserController.java
+++ src/main/java/com/example/BlindCafe/controller/UserController.java
@@ -26,67 +26,67 @@ public class UserController {
      * 마이페이지 (유저 정보 조회)
      */
     @GetMapping
-    public UserDetailDto getUserDetail(Authentication authentication) {
+    public ResponseEntity<UserDetailDto> getUserDetail(Authentication authentication) {
         log.info("GET /api/user");
-        return userService.getUserDetail(getUserId(authentication));
+        return ResponseEntity.ok(userService.getUserDetail(getUserId(authentication)));
     }
 
     /**
      * 유저 정보 추가 입력(온보딩)
      */
     @PostMapping
-    public CreateUserInfoDto.Response addUserInfo(
+    public ResponseEntity<CreateUserInfoDto.Response> addUserInfo(
             Authentication authentication,
             @Valid @RequestBody CreateUserInfoDto.Request request
     ) {
         log.info("POST /api/user");
         User user = (User) authentication.getPrincipal();
-        return userService.addUserInfo(user, request);
+        return ResponseEntity.ok(userService.addUserInfo(user, request));
     }
 
     /**
      * 홈화면 (유저 매칭 상태 확인)
      */
     @GetMapping("/home")
-    public UserHomeDto.Response userHome(Authentication authentication) {
+    public ResponseEntity<UserHomeDto.Response> userHome(Authentication authentication) {
         log.info("GET /api/user/home");
-        return userService.userHome(getUserId(authentication));
+        return ResponseEntity.ok(userService.userHome(getUserId(authentication)));
     }
 
     /**
      * 유저 관심사 수정
      */
     @PutMapping("/interest")
-    public EditInterestDto.Response editInterest(
+    public ResponseEntity<EditInterestDto.Response> editInterest(
             Authentication authentication,
             @Valid @RequestBody EditInterestDto.Request request
     ) {
         log.info("PUT /api/user/interest");
-        return userService.editInterest(getUserId(authentication), request);
+        return ResponseEntity.ok(userService.editInterest(getUserId(authentication), request));
     }
 
     /**
      * 유저 닉네임 수정
      */
     @PatchMapping("/nickname")
-    public EditNicknameDto.Response editNickname (
+    public ResponseEntity<EditNicknameDto.Response> editNickname (
             Authentication authentication,
             @Valid @RequestBody EditNicknameDto.Request request
     ) {
         log.info("PATCH /api/user/nickname");
-        return userService.editNickname(getUserId(authentication), request);
+        return ResponseEntity.ok(userService.editNickname(getUserId(authentication), request));
     }
 
     /**
      * 유저 주소 수정
      */
     @PatchMapping("/address")
-    public EditAddressDto.Response editAddress(
+    public ResponseEntity<EditAddressDto.Response> editAddress(
             Authentication authentication,
             @Valid @RequestBody EditAddressDto.Request request
     ) {
         log.info("PATCH /api/user/address");
-        return userService.editAddress(getUserId(authentication), request);
+        return ResponseEntity.ok(userService.editAddress(getUserId(authentication), request));
     }
 
     /**
@@ -120,11 +120,11 @@ public class UserController {
      * 유저 탈퇴
      */
     @DeleteMapping
-    public DeleteUserDto.Response deleteUser(
+    public ResponseEntity<DeleteUserDto.Response> deleteUser(
             Authentication authentication,
             @RequestParam(value="reason", defaultValue="1") Long reasonNum
     ) {
         log.info("DELETE /api/user");
-        return userService.deleteUser(getUserId(authentication), reasonNum);
+        return ResponseEntity.ok(userService.deleteUser(getUserId(authentication), reasonNum));
     }
 }
