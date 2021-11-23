e75ad2600fcbe8864da2097b4ad99611394edfda
Heedong Kim <ruthetum@gmail.com>
2021-11-23 22:35:49 +0900

    [fix] 거절 사유 문구 수정

--- src/main/java/com/example/BlindCafe/type/ReasonType.java
+++ src/main/java/com/example/BlindCafe/type/ReasonType.java
@@ -9,7 +9,7 @@ public enum ReasonType {
 
     FOR_LEAVE_ROOM("방 나가는 이유"),
     FOR_REPORT("신고 이유"),
-    FOR_WONT_EXCHANGE_PROFILE("프로필 교환 안 하는 이유"),
+    FOR_WONT_EXCHANGE_PROFILE("프로필 교환 거절 이유"),
     FOR_RETIRED("탈퇴 이유");
     
     private final String description;
