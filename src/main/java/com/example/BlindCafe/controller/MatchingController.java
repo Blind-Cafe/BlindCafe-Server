6ff87de63189d10c45c176f4f3d604aa4fa38016
Heedong Kim <ruthetum@gmail.com>
2021-11-28 21:17:48 +0900

    [feat] 매칭 컨트롤러 response entity 추가

--- src/main/java/com/example/BlindCafe/controller/MatchingController.java
+++ src/main/java/com/example/BlindCafe/controller/MatchingController.java
@@ -24,56 +24,56 @@ public class MatchingController {
      * 내 테이블 조회 - 프로필 교환을 완료한 상대방 목록 조회
      */
     @GetMapping
-    public MatchingListDto getMatchings(Authentication authentication) {
+    public ResponseEntity<MatchingListDto> getMatchings(Authentication authentication) {
         log.info("GET /api/matching");
-        return matchingService.getMatchings(getUserId(authentication));
+        return ResponseEntity.ok(matchingService.getMatchings(getUserId(authentication)));
     }
 
     /**
      * 채팅방 정보 조회
      */
     @GetMapping("{matchingId}")
-    public MatchingDetailDto getMatching(
+    public ResponseEntity<MatchingDetailDto> getMatching(
             Authentication authentication,
             @PathVariable Long matchingId
     )  {
         log.info("GET /api/matching/{}", matchingId);
-        return matchingService.getMatching(getUserId(authentication), matchingId);
+        return ResponseEntity.ok(matchingService.getMatching(getUserId(authentication), matchingId));
     }
 
     /**
      * 매칭 요청하기
      */
     @PostMapping
-    public CreateMatchingDto.Response createMatching(Authentication authentication) {
+    public ResponseEntity<CreateMatchingDto.Response> createMatching(Authentication authentication) {
         log.info("POST /api/matching");
-        return matchingService.createMatching(getUserId(authentication));
+        return ResponseEntity.ok(matchingService.createMatching(getUserId(authentication)));
     }
 
     /**
      * 채팅방 나가기
      */
     @DeleteMapping("{matchingId}")
-    public DeleteMatchingDto deleteMatching(
+    public ResponseEntity<DeleteMatchingDto> deleteMatching(
             Authentication authentication,
             @PathVariable Long matchingId,
             @RequestParam(value = "reason") Long reasonNum
     ) {
         log.info("DELETE /api/matching/{}", matchingId);
-        return matchingService.deleteMatching(getUserId(authentication), matchingId, reasonNum);
+        return ResponseEntity.ok(matchingService.deleteMatching(getUserId(authentication), matchingId, reasonNum));
     }
 
     /**
      * 음료수 설정하기
      */
     @PostMapping("{matchingId}/drink")
-    public DrinkDto.Response setDrink(
+    public ResponseEntity<DrinkDto.Response> setDrink(
             Authentication authentication,
             @PathVariable Long matchingId,
             @Valid @RequestBody DrinkDto.Request request
     ) {
         log.info("POST /api/matching/{}/drink", matchingId);
-        return matchingService.setDrink(getUserId(authentication), matchingId, request);
+        return ResponseEntity.ok(matchingService.setDrink(getUserId(authentication), matchingId, request));
     }
 
     /**
@@ -114,6 +114,23 @@ public class MatchingController {
         return ResponseEntity.ok(
                 matchingService.getPartnerProfile(getUserId(authentication), matchingId));
     }
+
+    /**
+     * 프로필 교환 수락하기
+     */
+    @PostMapping("{matchingId}/partner")
+    public ResponseEntity<Void> acceptPartnerProfile(
+            Authentication authentication,
+            @PathVariable Long matchingId
+    ) {
+        log.info("POST /api/matching/{}/partner", matchingId);
+        matchingService.acceptPartnerProfile(getUserId(authentication), matchingId);
+        return ResponseEntity.ok().build();
+    }
+
+    /**
+     * 프로필 교환 거절하기
+     */
 
     /**
      * 토픽 가져오기
