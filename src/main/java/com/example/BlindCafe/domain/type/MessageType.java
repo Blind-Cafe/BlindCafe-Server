04cabd546a1e283cf01af2f4862a6e6c34c211e3
Heedong Kim <ruthetum@gmail.com>
2022-03-28 17:30:41 +0900

    [feat] 메시지 타입에 비디오 추가

--- src/main/java/com/example/BlindCafe/domain/type/MessageType.java
+++ src/main/java/com/example/BlindCafe/domain/type/MessageType.java
@@ -9,11 +9,12 @@ public enum MessageType {
     TEXT(1, ""),
     IMAGE(2, "사진이 전송되었습니다."),
     AUDIO(3, "소리가 전송되었습니다."),
-    TEXT_TOPIC(4, "토픽이 전송되었습니다."),
-    IMAGE_TOPIC(5, "토픽이 전송되었습니다."),
-    AUDIO_TOPIC(6, "토픽이 전송되었습니다."),
-    DESCRIPTION(7, ""),
-    DRINK(8, "음료수가 선택되었습니다.");
+    VIDEO(4, "영상이 전송되었습니다."),
+    TEXT_TOPIC(5, "토픽이 전송되었습니다."),
+    IMAGE_TOPIC(6, "토픽이 전송되었습니다."),
+    AUDIO_TOPIC(7, "토픽이 전송되었습니다."),
+    DESCRIPTION(8, ""),
+    DRINK(9, "음료수가 선택되었습니다.");
 
     private final int type;
     private final String msg;
