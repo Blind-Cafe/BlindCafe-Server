0de93a6d9a18bd03c60822875a7ad8b9f0d5214c
Heedong Kim <ruthetum@gmail.com>
2022-03-28 17:34:24 +0900

    [feat] 사용 불가능한 닉네임 항목 추가

--- src/main/java/com/example/BlindCafe/service/UserService.java
+++ src/main/java/com/example/BlindCafe/service/UserService.java
@@ -67,6 +67,12 @@ public class UserService {
     private void validateAddUserInfo(AddUserInfoRequest request) {
         if (request.getMyGender().equals(N))
             throw new BlindCafeException(BAD_REQUEST);
+        
+        if (request.getNickname().contains("admin")
+            || request.getNickname().contains("관리자")
+            || request.getNickname().contains("manager")
+            || request.getNickname().contains("매니저"))
+            throw new BlindCafeException(INVALID_NICKNAME);
 
         Pattern pattern = Pattern.compile("^\\d{3}[- .]?\\d{4}[- .]?\\d{4}$");
         Matcher matcher = pattern.matcher(request.getPhone());
