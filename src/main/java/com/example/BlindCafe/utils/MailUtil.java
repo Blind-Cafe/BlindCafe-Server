a6c96f64086088a65a9f1851763a6a5c99c20aa3
Heedong Kim <ruthetum@gmail.com>
2022-03-30 12:43:29 +0900

    [fix] MailUtil 메시지 폼 만드는 append 문법 수정, 메일 수신 주소 수정

--- src/main/java/com/example/BlindCafe/utils/MailUtil.java
+++ src/main/java/com/example/BlindCafe/utils/MailUtil.java
@@ -19,7 +19,10 @@ import static com.example.BlindCafe.exception.CodeAndMessage.INTERNAL_SERVER_ERR
 public class MailUtil {
 
     @Value("${mail.host}")
-    private String host;
+    private String HOST;
+
+    @Value("${mail-destination}")
+    private String TO;
 
     private final JavaMailSender mailSender;
 
@@ -40,25 +43,22 @@ public class MailUtil {
         try {
             MimeMessage message = mailSender.createMimeMessage();
 
-            message.addRecipients(Message.RecipientType.TO, host);
+            message.addRecipients(Message.RecipientType.TO, TO);
             message.setSubject("[BlindCafe] 건의사항입니다.");
 
             StringBuilder msg = new StringBuilder();
             msg.append("<div style='margin:100px;'>");
-            msg.append("<h1> 건의사항 </h1>");
-            msg.append("<br>");
-            msg.append("<h3> 사용자명 : " + nickname + "</h3");
-            msg.append("<br>");
-            msg.append("<h3> 전화번호 : " + nickname + "</h3");
-            msg.append("<br>");
+            msg.append("<h1> 건의사항 </h1><br>");
+            msg.append("<h3> 사용자명 : ").append(nickname).append("</h3><br>");
+            msg.append("<h3> 전화번호 : ").append(phone).append("</h3><br>");
             msg.append("<br>");
             msg.append(content);
             msg.append("<br>");
             for (String image: images.split(","))
-                msg.append("<img src=" + image + "><br>");
+                msg.append("<img src=").append(image).append("><br>");
             msg.append("</div>");
             message.setText(msg.toString(), "utf-8", "html");
-            message.setFrom(new InternetAddress("dong011758@gmail.com","Heedong"));
+            message.setFrom(new InternetAddress(HOST,"Heedong"));
             return message;
         } catch (Exception e) {
             throw new BlindCafeException(EMAIL_SEND_ERROR);
